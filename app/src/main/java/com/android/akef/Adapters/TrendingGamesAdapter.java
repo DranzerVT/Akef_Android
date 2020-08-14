1086f35358940df31a8d50d2ff093b407933c5ae
Anoop Moothedath <anoop98moothedath@gmail.com>
2020-08-14 09:42:38 +0530

    Added Navigation Drawer, Integrated latest changes

--- app/src/main/java/com/android/akef/Adapters/TrendingGamesAdapter.java
+++ app/src/main/java/com/android/akef/Adapters/TrendingGamesAdapter.java
@@ -1,23 +1,18 @@
 package com.android.akef.Adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.akef.R;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class TrendingGamesAdapter extends RecyclerView.Adapter<TrendingGamesAdapter.ViewHolder> {
--- app/src/main/java/com/android/akef/UI/HomeFragment.java
+++ app/src/main/java/com/android/akef/UI/HomeFragment.java
@@ -1,21 +1,27 @@
 package com.android.akef.UI;
 
+import androidx.core.widget.NestedScrollView;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.android.akef.Adapters.TrendingGamesAdapter;
 import com.android.akef.R;
+import com.android.akef.Utils.Variables;
 import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
 
 public class HomeFragment extends Fragment {
@@ -25,6 +31,7 @@ public class HomeFragment extends Fragment {
     RecyclerView gameListView;
     TrendingGamesAdapter trendingGamesAdapter;
     private Context mContext;
+    Button btnAdminPanel, btnEsportsRegistration;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -43,9 +50,29 @@ public class HomeFragment extends Fragment {
         gameListView = view.findViewById(R.id.trend_games_list);
         gameListView.setLayoutManager(new GridLayoutManager(mContext,
                 1, GridLayoutManager.HORIZONTAL,false));
+        btnAdminPanel = view.findViewById(R.id.btn_admin_panel);
+        btnEsportsRegistration = view.findViewById(R.id.esports_reg_btn);
 
         trendingGamesAdapter = new TrendingGamesAdapter(mContext);
         gameListView.setAdapter(trendingGamesAdapter);
+
+        btnAdminPanel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://docs.google.com/document/d/1hMJhNwv0wKR4DHTV3SmY_LJFDQlZvJQMFnuEDdJLj7I/edit");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnEsportsRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext,WebViewActivity.class);
+                intent.putExtra(Variables.WEBVIEW_URL_KEY,"https://www.akef.in/esports-organization-registration/");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
